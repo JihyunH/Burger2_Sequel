fe890f143fd6976ce1bd9a16699610a0593499eb
Jihyun Gong <Jihyun Gong>
2017-04-10 23:16:09 -0400

    updated files

--- server.js
+++ server.js
@@ -3,22 +3,31 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 
+var db = require("./models");
+
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({
 	extended: false
-}))
+}));
 
 app.use(methodOverride('_method'));
+
 app.engine('handelbars', exphbs({
 	defaultLayout: 'Main'
 }));
 app.set('view engine', 'handelbars');
 
 var routes = require('./controllers/routes.js');
-app.use('/',routes);
 
-var port = 3000;
-app.listen(port);
+app.use('/', routes);
+app.use('/update', routes);
+app.use('/create', routes);
+
+var port = process.env.PORT || 3000;
+db.sequelize.sync().then(function(){
+	app.listen(port);
+});
+
 console.log("connected to server!");
